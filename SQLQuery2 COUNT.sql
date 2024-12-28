227aa1e70f45776ad347c28f37b3ee5df99cd337
Igor4358 <igor4358@yandex.ru>
2024-12-28 15:05:07 +0300

    Add files via upload

---
+++ SQLQuery2 COUNT.sql
@@ -0,0 +1,12 @@
+﻿USE PD_318_DML;
+
+SELECT
+      group_id AS 'ID',
+      group_name AS N'Название группы',
+      direction_name AS N'Направление обучения',
+      COUNT (student_id)
+
+      FROM Students,Groups,Directions
+      WHERE direction = direction_id
+      AND [group] = group_id
+      GROUP BY group_id,group_name,direction_name;
