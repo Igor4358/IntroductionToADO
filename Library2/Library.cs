1e63b89be0a6ca95f2f2d357678dc2ebb638aaec
Igor4358 <igor4358@yandex.ru>
2024-12-28 15:03:41 +0300

    Add files via upload

---
+++ Library2/Library.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Library2
+{
+   
+    internal class Library
+    {
+        public static string connectionString = " ";
+        static SqlConnection connection = null;
+        static readonly string delimiter1 = "\n-------------------------------------------";
+        static readonly string delimiter2 = "\n===========================================";
+        static Library()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            Console.WriteLine(connectionString);
+        }
+        public static void Select(string fields, string tables, string condition = "", int padding = 20)
+        {
+            string cmd = $"SELECT {fields} FROM {tables}";
+            if (condition.Length > 0) cmd += $" WHERE {condition}";
+            cmd += ";";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                Console.WriteLine(delimiter2);
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write(reader.GetName(i).PadRight(padding));
+                Console.WriteLine(delimiter1);
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        Console.Write(reader[i].ToString().PadRight(padding));
+                    Console.WriteLine();
+                }
+                Console.WriteLine(delimiter2);
+            }
+            reader.Close();
+            connection.Close();
+        }
+        public static void Insert(string table, string fields, string values)
+        {
+            string cmd = $"INSERT {table}({fields}) VALUES ({values});";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public static void InsertAuthor(int id, string last_name, string firs_name)
+        {
+            string cmd = "INSERT Authors(author_id, last_name, firs_name) VALUES(@id, @last_name, @firs_name)";
+            SqlCommand command = new SqlCommand(cmd, connection);
+
+            SqlParameter p_id = new SqlParameter("@id", SqlDbType.Int); p_id.Value = id;
+            SqlParameter p_last_name = new SqlParameter("@last_name", SqlDbType.NVarChar, 150); p_last_name.Value = last_name;
+            SqlParameter p_firs_name = new SqlParameter("@firs_name", SqlDbType.NVarChar, 150); p_firs_name.Value = firs_name;
+
+            command.Parameters.Add(p_id);
+            command.Parameters.Add(p_last_name);
+            command.Parameters.Add(p_firs_name);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public static void InsertBook(int id, string title, int size, string date, string author)
+        {
+            string cmd =
+                "INSERT Books(book_id,book_title,book_size,publish_date,author) " +
+                "VALUES (@id,@title,@size,@date,@author)";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            command.Parameters.Add("@title", SqlDbType.NVarChar, 256).Value = title;
+            command.Parameters.Add("@size", SqlDbType.Int).Value = size;
+            command.Parameters.Add("@date", SqlDbType.Date).Value = date;
+            command.Parameters.Add("@author", SqlDbType.Int).Value = GetAuthorID(author);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public static int GetAuthorID(string full_name)
+        {
+            string cmd =
+                "SELECT author_id FROM Authors WHERE last_name=@last_name AND firs_name=@firs_name";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            command.Parameters.Add("@firs_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').First();
+            command.Parameters.Add("@last_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').Last();
+            connection.Open();
+            int id = Convert.ToInt32(command.ExecuteScalar());
+            connection.Close();
+            return id;
+        }
+    }
+}
---
+++ Library2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Library2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Library.GetAuthorID("Bjarne Stroustrup"));
+            //Library.InsertBook(7, "C++ Programming Language", 331, "1986-01-29", "Bjarne Stroustrup");
+            Library.Select("author_id,firs_name,last_name", "Authors");
+            Library.Select
+                (
+                    "book_id,book_title,publish_date,[Author]=firs_name+' '+last_name",
+                    "Books,Authors",
+                    "author=author_id;",
+                    32
+                );
+        }
+    }
+}
